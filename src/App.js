61d22300c3845ec981bebcf21481b413b240f923
777shetty <shetty.chetu7@gmail.com>
2021-11-10 16:12:04 +0530

    ADDED:validation for user input

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component{
 constructor(){
   super()
   this.state={
-    userName:''
+    userName:'',
+    nameError:''
   }
 }
 onClick =($event)=>{
@@ -16,11 +17,17 @@ onClick =($event)=>{
 }
 onNameChange=(event)=>{
   console.log("value is",event.target.value);
+  const nameRegex=RegExp('^[A-Z]{1}[a-zA-z\\s]{2,}$');
   this.setState({userName:event.target.value})
+  if(nameRegex.test(event.target.value)){
+    this.setState({nameError:''})
+  }else{
+    this.setState({nameError:'Name is Incorrect'})
+  }
 }
   render(){
     return(
-      <>
+      <div>
       <div>
         <h1>Hello {this.state.userName}from bridgelabz</h1>
         <img src={logo} onClick={this.onClick}
@@ -28,8 +35,9 @@ onNameChange=(event)=>{
       </div>
       <div>
         <input onChange={this.onNameChange}/>
+        <span className="error-output">{this.state.nameError}</span>
       </div>
-      </>
+      </div>
     );
   }
 }
