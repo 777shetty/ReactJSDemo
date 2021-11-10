99bd960b15c987c51b3bb104818f56ac1b66b83f
777shetty <shetty.chetu7@gmail.com>
2021-11-10 15:10:10 +0530

    ADDED:display bridgelabz logo using peoperty binding technique

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ constructor(){
     return(
       <div>
         <h1>{this.state.title}</h1>
+        <img src={logo192.png}
+        alt="The BridgelabzLogo: a bridge to employement through tab works"/>
       </div>
     );
   }
